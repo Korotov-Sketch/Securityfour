49558706a7b386251cf4c93f0f54c3fe962e9506
Korotov-Sketch <olegkorotof@mail.ru>
2021-02-05 13:18:16 +0400

    Styled

---
+++ src/main/java/Decodonator.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Decodonator {
+    public static void main(String[] args) throws Exception {
+        FileReader fileReader = new FileReader("src/main/resources/TextWithWord.txt");
+        BufferedReader readerTextWithWord = new BufferedReader(fileReader);
+
+        StringBuilder textWithWord = new StringBuilder();
+        String line = readerTextWithWord.readLine();
+        textWithWord.append(line);
+        while (line != null) {
+            line = readerTextWithWord.readLine();
+            if (line != null) {
+                textWithWord.append("\n" + line);
+            }
+        }
+        readerTextWithWord.close();
+
+        StringBuilder stringBits = new StringBuilder();
+        char[] textWithWordChars = textWithWord.toString().toCharArray();
+        for (int i = 0, j = 0; i < textWithWordChars.length && (j < 8 || stringBits.length() % 8 != 0); i++) {
+            if (textWithWordChars[i] == 'у' || textWithWordChars[i] == 'К' || textWithWordChars[i] == 'е' || 
+                    textWithWordChars[i] == 'Е' || textWithWordChars[i] == 'Н' || textWithWordChars[i] == 'х' ||
+                    textWithWordChars[i] == 'Х' || textWithWordChars[i] == 'В' || textWithWordChars[i] == 'а' || 
+                    textWithWordChars[i] == 'А' || textWithWordChars[i] == 'р' || textWithWordChars[i] == 'Р' ||
+                    textWithWordChars[i] == 'о' || textWithWordChars[i] == 'О' || textWithWordChars[i] == 'с' || 
+                    textWithWordChars[i] == 'С' || textWithWordChars[i] == 'М' || textWithWordChars[i] == 'Т') {
+                stringBits.append("1");
+                j = 0;
+            }
+            if (textWithWordChars[i] == 'e' || textWithWordChars[i] == 'E' ||textWithWordChars[i] == 'T' || 
+                    textWithWordChars[i] == 'y' || textWithWordChars[i] == 'o' || textWithWordChars[i] == 'O' ||
+                    textWithWordChars[i] == 'p' || textWithWordChars[i] == 'P' || textWithWordChars[i] == 'a' || 
+                    textWithWordChars[i] == 'A' || textWithWordChars[i] == 'H' || textWithWordChars[i] == 'K' ||
+                    textWithWordChars[i] == 'x' || textWithWordChars[i] == 'X' || textWithWordChars[i] == 'c' ||
+                    textWithWordChars[i] == 'C' || textWithWordChars[i] == 'B' || textWithWordChars[i] == 'M') {
+                stringBits.append(0);
+                j++;
+            }
+        }
+
+        char[] bits = stringBits.delete(stringBits.length() - 8, stringBits.length())
+                .toString()
+                .toCharArray();
+
+        byte[] bytesVar = new byte[bits.length / 8];
+        int byteFromBits = 0;
+        for (int i = 0, deg = 7; i < bits.length; i++) {
+            byteFromBits += Math.pow(2, deg) * Character.getNumericValue(bits[i]);
+
+            if (deg == 0) {
+                deg = 7;
+                bytesVar[i / 8] = (byte)byteFromBits;
+                byteFromBits = 0;
+            }
+            else {
+                deg--;
+            }
+        }
+
+        String FinalWord = new String(bytesVar);
+        System.out.println(FinalWord);
+    }
+}
---
+++ src/main/java/Encryptionator.java
@@ -0,0 +1,129 @@
+import java.io.*;
+
+public class Encryptionator {
+    public static void main(String[] args) throws Exception {
+        FileInputStream inputIO = new FileInputStream("src/main/resources/Word.txt");
+
+        FileReader fileReader = new FileReader("src/main/resources/Text.txt");
+        BufferedReader TXTReader = new BufferedReader(fileReader);
+
+        FileWriter fileWriter = new FileWriter("src/main/resources/TextWithWord.txt");
+        BufferedWriter TxtWriter = new BufferedWriter(fileWriter);
+
+        StringBuilder txt = new StringBuilder();
+        String lin = TXTReader.readLine();
+        txt.append(lin);
+        while (lin != null) {
+            lin = TXTReader.readLine();
+            if (lin != null) { txt.append("\n" + lin); }
+        }
+        TXTReader.close();
+
+        byte[] bytes = inputIO.readAllBytes();
+        StringBuilder bitses = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            bitses.append(String.format("%8s", Integer.toBinaryString(bytes[i])).replace(" ", "0"));
+        }
+
+        char[] BitsOfSymbols = bitses.toString().toCharArray();
+        char[] SymbolsOfText = txt.toString().toCharArray();
+        int pos = 0;
+        for (int i = 0; i < BitsOfSymbols.length; i++) {
+            for (int j = pos; j < SymbolsOfText.length; j++) {
+                if (SymbolsOfText[j] == 'e') {
+                    pos = j + 1;
+                    if (BitsOfSymbols[i] == '1') { SymbolsOfText[j] = 'е';}
+                    break;
+                }
+                if (SymbolsOfText[j] == 'E') {
+                    pos = j + 1;
+                    if (BitsOfSymbols[i] == '1') { SymbolsOfText[j] = 'Е'; }
+                    break;
+                }
+                if (SymbolsOfText[j] == 'T') {
+                    pos = j + 1;
+                    if (BitsOfSymbols[i] == '1') { SymbolsOfText[j] = 'Т'; }
+                    break;
+                }
+                if (SymbolsOfText[j] == 'y') {
+                    pos = j + 1;
+                    if (BitsOfSymbols[i] == '1') { SymbolsOfText[j] = 'у'; }
+                    break;
+                }
+                if (SymbolsOfText[j] == 'o') {
+                    pos = j + 1;
+                    if (BitsOfSymbols[i] == '1') { SymbolsOfText[j] = 'о'; }
+                    break;
+                }
+                if (SymbolsOfText[j] == 'O') {
+                    pos = j + 1;
+                    if (BitsOfSymbols[i] == '1') { SymbolsOfText[j] = 'О'; }
+                    break;
+                }
+                if (SymbolsOfText[j] == 'p') {
+                    pos = j + 1;
+                    if (BitsOfSymbols[i] == '1') { SymbolsOfText[j] = 'р'; }
+                    break;
+                }
+                if (SymbolsOfText[j] == 'P') {
+                    pos = j + 1;
+                    if (BitsOfSymbols[i] == '1') { SymbolsOfText[j] = 'Р'; }
+                    break;
+                }
+                if (SymbolsOfText[j] == 'a') {
+                    pos = j + 1;
+                    if (BitsOfSymbols[i] == '1') { SymbolsOfText[j] = 'а'; }
+                    break;
+                }
+                if (SymbolsOfText[j] == 'A') {
+                    pos = j + 1;
+                    if (BitsOfSymbols[i] == '1') { SymbolsOfText[j] = 'А'; }
+                    break;
+                }
+                if (SymbolsOfText[j] == 'H') {
+                    pos = j + 1;
+                    if (BitsOfSymbols[i] == '1') { SymbolsOfText[j] = 'Н'; }
+                    break;
+                }
+                if (SymbolsOfText[j] == 'K') {
+                    pos = j + 1;
+                    if (BitsOfSymbols[i] == '1') { SymbolsOfText[j] = 'К'; }
+                    break;
+                }
+                if (SymbolsOfText[j] == 'x') {
+                    pos = j + 1;
+                    if (BitsOfSymbols[i] == '1') { SymbolsOfText[j] = 'х'; }
+                    break;
+                }
+                if (SymbolsOfText[j] == 'X') {
+                    pos = j + 1;
+                    if (BitsOfSymbols[i] == '1') { SymbolsOfText[j] = 'Х'; }
+                    break;
+                }
+                if (SymbolsOfText[j] == 'c') {
+                    pos = j + 1;
+                    if (BitsOfSymbols[i] == '1') { SymbolsOfText[j] = 'с'; }
+                    break;
+                }
+                if (SymbolsOfText[j] == 'C') {
+                    pos = j + 1;
+                    if (BitsOfSymbols[i] == '1') { SymbolsOfText[j] = 'с'; }
+                    break;
+                }
+                if (SymbolsOfText[j] == 'B') {
+                    pos = j + 1;
+                    if (BitsOfSymbols[i] == '1') { SymbolsOfText[j] = 'В'; }
+                    break;
+                }
+                if (SymbolsOfText[j] == 'M') {
+                    pos = j + 1;
+                    if (BitsOfSymbols[i] == '1') { SymbolsOfText[j] = 'М'; }
+                    break;
+                }
+            }
+        }
+
+        TxtWriter.write(SymbolsOfText);
+        TxtWriter.close();
+    }
+}
